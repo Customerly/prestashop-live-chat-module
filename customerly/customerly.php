203c5adf98e8a7226634e77f4f5ab1a1ad73b83c
Igor Zanella <igorzanella.dev@gmail.com>
2021-05-11 20:28:19 +0200

    feat: add new snippet code
    a

--- customerly/customerly.php
+++ customerly/customerly.php
@@ -161,7 +161,8 @@ class Customerly extends Module
     {
         if (!$this->isCached('module:customerly/views/templates/hook/header.tpl', $this->getCacheId())) {
             $project_id = Configuration::get('CLY_PROJECT_ID');
-            $live_chat_snippet = '<!-- Customerly Integration Code --><script>window.customerlySettings = {app_id: "' . $project_id . '"};!function(){function e(){var e=t.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://widget.customerly.io/widget/' . $project_id . '";var r=t.getElementsByTagName("script")[0];r.parentNode.insertBefore(e,r)}var r=window,t=document,n=function(){n.c(arguments)};r.customerly_queue=[],n.c=function(e){r.customerly_queue.push(e)},r.customerly=n,r.attachEvent?r.attachEvent("onload",e):r.addEventListener("load",e,!1)}();</script>';
+            $live_chat_snippet = '<!-- Customerly Live Chat Snippet Code --><script>!function(){var e=window,i=document,t="customerly",n="queue",o="load",r="settings",u=e[t]=e[t]||[];if(u.t){return void u.i("[customerly] SDK already initialized. Snippet included twice.")}u.t=!0;u.loaded=!1;u.o=["event","attribute","update","show","hide","open","close"];u[n]=[];u.i=function(t){e.console&&!u.debug&&console.error&&console.error(t)};u.u=function(e){return function(){var t=Array.prototype.slice.call(arguments);return t.unshift(e),u[n].push(t),u}};u[o]=function(t){u[r]=t||{};if(u.loaded){return void u.i("[customerly] SDK already loaded. Use customerly.update to change settings.")}u.loaded=!0;var e=i.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://messenger.customerly.io/launcher.js";var n=i.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};u.o.forEach(function(t){u[t]=u.u(t)})}();customerly.load({"app_id":"' . $project_id . '"});</script><!-- End of Customerly Live Chat Snippet Code -->';
+
             $this->context->smarty->assign('customerly', array(
                 'head_code' => $live_chat_snippet,
             ));
